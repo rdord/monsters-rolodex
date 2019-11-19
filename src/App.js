aade3e8834e9530b89c1ad003565c29beffc0a0d
Roland Đorđević <roland.djordjevic@ineor.si>
2019-11-19 20:12:23 +0100

    refactor to react hook

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      msg: 'Default message'
-    }
-  }
+function App() {
+  const [msg, setMsg] = useState('Default message');
 
-  render() {
-    return (
-      <div className="App">
+  return (
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>{this.state.msg}</p>
-        <button onClick={() => this.setState({msg: 'New message'})}>Change message</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>{msg}</p>
+        <button onClick={() => setMsg('New new message')}>CHANGE MSG</button>
       </header>
     </div>
-    );
-  }
+  );
 }
 
 export default App;
