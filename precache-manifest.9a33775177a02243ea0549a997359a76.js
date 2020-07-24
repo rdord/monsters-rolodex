74dc80ef7420d2beaea9f22756c609fd5e116fb0
Roland Đorđević <roland.djordjevic@ineor.si>
2020-07-24 23:59:09 +0200

    Updates

---
+++ precache-manifest.9a33775177a02243ea0549a997359a76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f91da532615cc04451c4494d6217a4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "52b3e74d6f45c7f79c1d",
+    "url": "/monsters-rolodex/static/css/main.b2820a74.chunk.css"
+  },
+  {
+    "revision": "1da0fb73ee011a7353e5",
+    "url": "/monsters-rolodex/static/js/2.046b8bd3.chunk.js"
+  },
+  {
+    "revision": "52b3e74d6f45c7f79c1d",
+    "url": "/monsters-rolodex/static/js/main.4ede0715.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
